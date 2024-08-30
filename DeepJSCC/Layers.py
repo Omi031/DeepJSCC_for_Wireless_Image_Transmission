48f2b8aa02097f83325974d5516f3e56c3d6de27
Omi <naoki.omi031@gmail.com>
2024-08-30 18:39:57 +0900

    update

--- DeepJSCC/Layers.py
+++ DeepJSCC/Layers.py
@@ -2,7 +2,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras import layers
 
-'''
+"""
 Normalization
   Normalize pixel values according to power constraint P.
 
@@ -10,29 +10,32 @@ k       : Number of pixels per image
 P       : Power constraint (default P = 1)
 x_norm  : Norm of pixel values per image
 p       : Poser per image
-'''
+"""
+
+
 class Normalization(layers.Layer):
-  def __init__(self, k, P, **kwargs):
-    super(Normalization, self).__init__()
-    self.k = k
-    self.P = P
-  def call(self, x):
-    x_shape = tf.shape(x)
-    x_norm = tf.sqrt(tf.reduce_sum(tf.square(x), axis=[1,2,3], keepdims=True))
-    x_norm = tf.broadcast_to(x_norm, x_shape)
-    p = np.sqrt(self.k * self.P)
-    y = p*x/x_norm
-    return y
-  def get_config(self):
-    config = super(Normalization, self).get_config()
-    config.update({
-      'k': self.k,
-      'P': self.P
-    })
-    return config
-  @classmethod
-  def from_config(cls, config):
-    return cls(**config)
+    def __init__(self, k, P, **kwargs):
+        super(Normalization, self).__init__()
+        self.k = k
+        self.P = P
+
+    def call(self, x):
+        x_shape = tf.shape(x)
+        x_norm = tf.sqrt(tf.reduce_sum(tf.square(x), axis=[1, 2, 3], keepdims=True))
+        x_norm = tf.broadcast_to(x_norm, x_shape)
+        p = np.sqrt(self.k * self.P)
+        y = p * x / x_norm
+        return y
+
+    def get_config(self):
+        config = super(Normalization, self).get_config()
+        config.update({"k": self.k, "P": self.P})
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
+
 
 # class Normalization(layers.Layer):
 #   def __init__(self, k, P, **kwargs):
@@ -47,7 +50,7 @@ class Normalization(layers.Layer):
 #     # x_norm = tf.expand_dims(x_norm, axis=3)
 #     x_norm = tf.broadcast_to(x_norm, x_shape)
 #     # x_norm = tf.tile(x_norm, [1,x_shape[1], x_shape[2], 1])
-    
+
 #     sqrt_kP = np.sqrt(self.k * self.P)
 #     y = sqrt_kP*tf.divide(x, x_norm)
 #     return y
@@ -62,7 +65,7 @@ class Normalization(layers.Layer):
 #   def from_config(cls, config):
 #     return cls(**config)
 
-'''
+"""
 AWGN channel
 x       : input
           shape = (Batch,8,8,c)
@@ -77,28 +80,34 @@ noise   : AWGN noise
 
 stddev  : Standard deviation of real noise distribution
 N       : Variance of complex noise distribution
-'''
+"""
+
+
 class AWGN_Channel(layers.Layer):
-  def __init__(self, N, **kwargs):
-    super(AWGN_Channel, self).__init__()
-    # Divde N by 2 because we consider real noise
-    self.stddev = np.sqrt(N/2)
-  def call(self, x):
-    with tf.name_scope('AWGN_Channel'):
-      # AWGN noise
-      noise = tf.random.normal(tf.shape(x), mean=0.0, stddev=self.stddev, dtype=tf.float32)
-      y = x + noise
-    return y
-  def get_config(self):
-    config = super(AWGN_Channel, self).get_config()
-    config.update({
-      'N': self.stddev
-    })
-    return config
-  @classmethod
-  def from_config(cls, config):
-    return cls(**config)
-  
+    def __init__(self, N, **kwargs):
+        super(AWGN_Channel, self).__init__()
+        # Divde N by 2 because we consider real noise
+        self.stddev = np.sqrt(N / 2)
+
+    def call(self, x):
+        with tf.name_scope("AWGN_Channel"):
+            # AWGN noise
+            noise = tf.random.normal(
+                tf.shape(x), mean=0.0, stddev=self.stddev, dtype=tf.float32
+            )
+            y = x + noise
+        return y
+
+    def get_config(self):
+        config = super(AWGN_Channel, self).get_config()
+        config.update({"N": self.stddev})
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
+
+
 # class Slow_Rayleigh_Fading_Channel(layers.Layer):
 #   def __init__(self, **kwargs):
 #     super(Slow_Rayleigh_Fading_Channel, self).__init__()
@@ -114,18 +123,17 @@ class AWGN_Channel(layers.Layer):
 #       x_i = tf.concat([zeros, x], axis=2)
 #       x_q = tf.concat([x, zeros], axis=2)
 #       print(x_i.shape)
-      
+
 #       h_i = tf.random.normal(mean=0.0, stddev=self.stddevs, dtype=tf.float32)
 #       h_q = tf.random.normal(mean=0.0, stddev=self.stddevs, dtype=tf.float32)
 
-      
 
 #     return x
 #   @classmethod
 #   def from_config(cls, config):
 #     return cls(**config)
 
-'''
+"""
 Slow Rayleigh Fading Channel
 x   : input 
       shape = (Batch,8,8,c)
@@ -142,40 +150,56 @@ h_i : I components of h
 
 h_q : Q components of h
       h_q~N(0,0.5)
-'''
+"""
+
+
 class Slow_Rayleigh_Fading_Channel(layers.Layer):
-  def __init__(self, **kwargs):
-    super(Slow_Rayleigh_Fading_Channel, self).__init__()
-    # Fading variance is 1/2 because we consider real values (not complex)
-    self.stddevs = np.sqrt(1)
-    # Matrix for extracting even/odd columns of feature maps
-    idx_e = tf.constant([[[1,0]]], dtype=tf.float32)
-    idx_o = tf.constant([[[0,1]]], dtype=tf.float32)
-    self.idx_e = tf.expand_dims(idx_e, -1)
-    self.idx_o = tf.expand_dims(idx_o, -1)
-  def call(self, x):
-    with tf.name_scope('Slow_Rayleigh_Fading_Channel'):
-      x_shape = tf.shape(x)
-      # Matrix for extracting even/odd columns of feature maps
-      idx_e = tf.tile(self.idx_e, [x_shape[0], x_shape[1], x_shape[2]//2, x_shape[3]])
-      idx_o = tf.tile(self.idx_o, [x_shape[0], x_shape[1], x_shape[2]//2, x_shape[3]])
-      # Define even/odd columns of feature maps of x as I/Q components
-      x_i = x[:,:,0::2,:]
-      x_q = x[:,:,1::2,:]
-      # Rayleigh fading coefficient
-      h_i = tf.random.normal([x_shape[0], 1, 1, 1], mean=0.0, stddev=self.stddevs, dtype=tf.float32)
-      h_q = tf.random.normal([x_shape[0], 1, 1, 1], mean=0.0, stddev=self.stddevs, dtype=tf.float32)  
-      # y = h*x
-      y_i = h_i*x_i - h_q*x_q
-      y_q = h_q*x_i + h_i*x_q
-      # Combine I and Q components
-      y_i2 = tf.gather(y_i, [0,0,1,1,2,2,3,3], axis=2)
-      y_q2 = tf.gather(y_q, [0,0,1,1,2,2,3,3], axis=2)
-      y = y_i2*idx_e + y_q2*idx_o
-    return y
-  @classmethod
-  def from_config(cls, config):
-    return cls(**config)
+    def __init__(self, N, **kwargs):
+        super(Slow_Rayleigh_Fading_Channel, self).__init__()
+        # Slow Rayleigh fading
+        # Fading variance is 1/2 because we consider real values (not complex)
+        self.stddevs = np.sqrt(1)
+        # Matrix for extracting even/odd columns of feature maps
+        idx_e = tf.constant([[[1, 0]]], dtype=tf.float32)
+        idx_o = tf.constant([[[0, 1]]], dtype=tf.float32)
+        self.idx_e = tf.expand_dims(idx_e, -1)
+        self.idx_o = tf.expand_dims(idx_o, -1)
+
+        #
+
+    def call(self, x):
+        with tf.name_scope("Slow_Rayleigh_Fading_Channel"):
+            x_shape = tf.shape(x)
+            # Matrix for extracting even/odd columns of feature maps
+            idx_e = tf.tile(
+                self.idx_e, [x_shape[0], x_shape[1], x_shape[2] // 2, x_shape[3]]
+            )
+            idx_o = tf.tile(
+                self.idx_o, [x_shape[0], x_shape[1], x_shape[2] // 2, x_shape[3]]
+            )
+            # Define even/odd columns of feature maps of x as I/Q components
+            x_i = x[:, :, 0::2, :]
+            x_q = x[:, :, 1::2, :]
+            # Rayleigh fading coefficient
+            h_i = tf.random.normal(
+                [x_shape[0], 1, 1, 1], mean=0.0, stddev=self.stddevs, dtype=tf.float32
+            )
+            h_q = tf.random.normal(
+                [x_shape[0], 1, 1, 1], mean=0.0, stddev=self.stddevs, dtype=tf.float32
+            )
+            # y = h*x
+            y_i = h_i * x_i - h_q * x_q
+            y_q = h_q * x_i + h_i * x_q
+            # Combine I and Q components
+            y_i2 = tf.gather(y_i, [0, 0, 1, 1, 2, 2, 3, 3], axis=2)
+            y_q2 = tf.gather(y_q, [0, 0, 1, 1, 2, 2, 3, 3], axis=2)
+            y = y_i2 * idx_e + y_q2 * idx_o
+        return y
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
+
 
 # class Slow_Rayleigh_Fading_Channel(layers.Layer):
 #   def __init__(self, **kwargs):
@@ -202,7 +226,7 @@ class Slow_Rayleigh_Fading_Channel(layers.Layer):
 #     super(Slow_Rayleigh_Fading_Channel, self).__init__()
 #   def random_normal(self):
 #     return tf.random.normal([1, 1], mean=0.0, stddev=1.0, dtype=tf.float32)
-  
+
 #   @tf.function
 #   def call(self, x):
 #     with tf.name_scope('Slow_Rayleigh_Fading_Channel'):
@@ -268,7 +292,7 @@ class Slow_Rayleigh_Fading_Channel(layers.Layer):
 #     x_shape = x.shape
 
 #     y = tf.TensorArray(dtype=tf.float32, size=int(x.shape[3]))
-    
+
 #     y_real = tf.math.real(y_c)
 #     y_imag = tf.math.imag(y_c)
 #     for i in range(y_c.shape[3]):
@@ -287,32 +311,31 @@ class Slow_Rayleigh_Fading_Channel(layers.Layer):
 #       'P': self.P
 #     })
 #     return config
-  
+
 # class Normalization(layers.Layer):
-  # def __init__(self, k, P, **kwargs):
-  #   super(Normalization, self).__init__()
-  #   self.k = tf.cast(k, dtype=tf.complex128)
-  #   self.P = tf.cast(P, dtype=tf.complex128)
-  # def call(self, z_tilta):
-  #   z_tilta = tf.cast(z_tilta, dtype=tf.complex128)
-  #   z_conjugateT = tf.math.conj(z_tilta, name='z_ConjugateTrans')
-  #   #Square root of k and P
-  #   sqrt1 = tf.dtypes.cast(tf.math.sqrt(self.k*self.P, name='NormSqrt1'), dtype='complex128',name='ComplexCastingNorm')
-  #   sqrt2 = tf.math.sqrt(z_conjugateT*z_tilta, name='NormSqrt2')#Square root of z_tilta* and z_tilta.
+# def __init__(self, k, P, **kwargs):
+#   super(Normalization, self).__init__()
+#   self.k = tf.cast(k, dtype=tf.complex128)
+#   self.P = tf.cast(P, dtype=tf.complex128)
+# def call(self, z_tilta):
+#   z_tilta = tf.cast(z_tilta, dtype=tf.complex128)
+#   z_conjugateT = tf.math.conj(z_tilta, name='z_ConjugateTrans')
+#   #Square root of k and P
+#   sqrt1 = tf.dtypes.cast(tf.math.sqrt(self.k*self.P, name='NormSqrt1'), dtype='complex128',name='ComplexCastingNorm')
+#   sqrt2 = tf.math.sqrt(z_conjugateT*z_tilta, name='NormSqrt2')#Square root of z_tilta* and z_tilta.
 
-  #   div = tf.math.divide(z_tilta,sqrt2, name='NormDivision')
-  #   #calculating channel input
-  #   z = tf.math.multiply(sqrt1,div, name='Z')  
-  #   z = tf.cast(z, dtype='float32') 
-  #   return z
-  # def get_config(self):
-  #   config = super(Normalization, self).get_config()
-  #   config.update({
-  #     'k': self.k,
-  #     'P': self.P
-  #   })
-  #   return config
-
+#   div = tf.math.divide(z_tilta,sqrt2, name='NormDivision')
+#   #calculating channel input
+#   z = tf.math.multiply(sqrt1,div, name='Z')
+#   z = tf.cast(z, dtype='float32')
+#   return z
+# def get_config(self):
+#   config = super(Normalization, self).get_config()
+#   config.update({
+#     'k': self.k,
+#     'P': self.P
+#   })
+#   return config
 
 
 # class Normalization(layers.Layer):
@@ -361,7 +384,7 @@ class Slow_Rayleigh_Fading_Channel(layers.Layer):
 #     y = y_real + tf.multiply(1j, y_imag)
 
 #     return y
-  
+
 # class Demodulation(layers.Layer):
 #   def __init__(self, **kwargs):
 #     super(Demodulation, self).__init__()
